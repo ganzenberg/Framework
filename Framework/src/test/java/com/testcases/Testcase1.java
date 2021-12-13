d1ec789f8a3b806e381c791733403ca70b5a8275
ganzenberg <kgan97esh@gmail.com>
2021-12-13 10:54:27 +0530

    yes

--- Framework/src/test/java/com/testcases/Testcase1.java
+++ Framework/src/test/java/com/testcases/Testcase1.java
@@ -9,7 +9,10 @@ public class Testcase1 extends BaseClass {
 
 @Test
 public void loginApp()
+// send key values
+
 {
+	
 
 	loginPage login = PageFactory.initElements(driver, loginPage.class);
 	login.loginToCRM(s1,s2);
